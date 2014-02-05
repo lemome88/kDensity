26f9b471b91121370bbcfb5d61145dff7a153dd6
letimome <leticia.montalvillo@ehu.es>
2014-02-05 14:04:47 +0100

    Update module_kdensity.R

--- module_kdensity.R
+++ module_kdensity.R
@@ -1,49 +1,33 @@
+####functiones para el FEATURE EPANECHNIKOV
+
+library(ggplot2) #Nueva libreria requerida
+
 ##Metodo principal
 kdensity<-function(sample,method,n=200,b=length(sample)^0.2){
   #Fijar la zona en la que calcular la densidad
   m=min(sample)-2*b
   M=max(sample)+2*b
   x.v=seq(from=m,to=M,by=(M-m)/n)
-  #Definir la funcion kernel a utilizar
   kernel=switch(method,
                 gauss=gaussian.kernel,
-                triangular=triangular.kernel)
-  #Obtener la densidad
+                triangular=triangular.kernel, ##Modificada (hay que reemplazar ')' por ',')
+                epanechnikov=epanechnikov.kernel)      ##Modificada (añadida)
   dens.v=sapply(x.v,FUN=kernel,sample=sample,b=b)/(length(sample)*b)
-  #Resultado final
   list(x=x.v,y=dens.v)
 }
 
-##FUNCIONES KERNEL
-##Funcion triangular
-triangular.kernel=function(x,sample,b){
-  s<-sample[abs(sample-x)<=b]
-  k<-0
-  if (length(s)>0){
-    u<-(x-s)/b
-    k<-sum(1-abs(u))  
-  }
-  k
-}
 
-##Funcion Gaussiana
-gaussian.kernel=function(x,sample,b){
+#Nuevo kernel aniadido (funcion nueva)
+epanechnikov.kernel<-function (x,sample,b){
   u<-(x-sample)/b
-  sum((1/sqrt(2*3.14159))*exp(-0.5*u^2))
+  u<-u[abs(u)<1]
+  k<-0
+  if (length(u)>0) k<-3/4*(1-u^2)
+  sum(k)
 }
 
-
-##FUNCIONES EXTRAS
-##Calculo aproximado de integrales
-numeric.integral<-function(data){
-  x<-data$x
-  y<-data$y
-  dx<-diff(x)
-  dy<-rowMeans(cbind(y[-1],y[-length(y)]))
-  sum(dx*dy)
-}
-
-##Representacion grafica
-plot.kernel<-function (data,col="red"){
-  plot(data,main="Kernel density estimation",xlab="X",ylab="Density",col=col,type="l",lwd=2)
+#Misma función extra pero totalmente reimplementada
+plot.kernel<-function(data,col="red",main="Kernel density estimation"){
+  df<-data.frame(data)
+  ggplot(df,aes(x=x,y=y)) + geom_line(col=col) + labs(x="X",y="Density",title=main)
 }
