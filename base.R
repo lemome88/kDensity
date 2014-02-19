654ca9bbc39fbf58035c5546a6f13fe083ed7417
lemome88 <lmontalvillo001@ehu.es>
2014-02-19 16:09:32 +0100

    Update base.R

--- base.R
+++ base.R
@@ -1,16 +1,12 @@
 ##Metodo principal
 kdensity<-function(sample,method,n=200,b=length(sample)^0.2){
-  #Fijar la zona en la que calcular la densidad
   m=min(sample)-2*b
   M=max(sample)+2*b
   x.v=seq(from=m,to=M,by=(M-m)/n)
-  #Definir la funcion kernel a utilizar
   kernel=switch(method,
                 gauss=gaussian.kernel,
                 triangular=triangular.kernel)
-  #Obtener la densidad
   dens.v=sapply(x.v,FUN=kernel,sample=sample,b=b)/(length(sample)*b)
-  #Resultado final
   list(x=x.v,y=dens.v)
 }
 
@@ -25,13 +21,11 @@ triangular.kernel=function(x,sample,b){
   }
   k
 }
-
 ##Funcion Gaussiana
 gaussian.kernel=function(x,sample,b){
   u<-(x-sample)/b
   sum((1/sqrt(2*3.14159))*exp(-0.5*u^2))
 }
-
 
 ##FUNCIONES EXTRAS
 ##Calculo aproximado de integrales
@@ -42,10 +36,6 @@ numeric.integral<-function(data){
   dy<-rowMeans(cbind(y[-1],y[-length(y)]))
   sum(dx*dy)
 }
-
-
-##
-
 ##Representacion grafica
 plot.kernel<-function (data,col="red"){
   plot(data,main="Kernel density estimation",xlab="X",ylab="Density",col=col,type="l",lwd=2)
@@ -54,14 +44,9 @@ plot.kernel<-function (data,col="red"){
 
 
 ##start of the program
-#Generacion de muestras aleatorias (usando una distribucion beta)
 muestra<-rbeta(10,2,200)
 muestra
-
-#Generacion de la estimacion basada en el kernel triangular
 triang.density<-kdensity(sample=muestra,method="triangular",n=200,b=0.005)
-#Representacion grafica
 x11()
 plot.kernel(triang.density)
-#Calculo del area (siempre deberia ser 1)
 numeric.integral(triang.density)
