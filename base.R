d511838a007dc96862d20b058684d5228a6b350d
lemome88 <lmontalvillo001@ehu.es>
2014-02-19 16:07:13 +0100

    Update base.R

--- base.R
+++ base.R
@@ -54,4 +54,14 @@ plot.kernel<-function (data,col="red"){
 
 
 ##start of the program
-df<-
+#Generacion de muestras aleatorias (usando una distribucion beta)
+muestra<-random.beta()
+muestra
+
+#Generacion de la estimacion basada en el kernel triangular
+triang.density<-kdensity(sample=muestra,method="triangular",n=200,b=0.005)
+#Representacion grafica
+x11()
+plot.kernel(triang.density)
+#Calculo del area (siempre deberia ser 1)
+numeric.integral(triang.density)
